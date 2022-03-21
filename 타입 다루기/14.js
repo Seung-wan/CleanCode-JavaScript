36f16c89264bd52c57435dd9973e6d5fdf68f958
Seung-wan <tmddhks0104@gmail.com>
2022-03-21 17:48:57 +0900

    fix. 타입다루기 2022-03-21

--- 타입 다루기/14.js
+++ 타입 다루기/14.js
@@ -14,6 +14,12 @@ isNaN;
 
 /*다
  * isNaN은 자동 형변환을 한다
+ * isNan()은 먼저 인수를 숫자로 형변환 후, NaN이면 true, 아니면 false
  * Number.isNaN은 형변환을 하지 않는다
  * Number.isNaN()은 강제로 매개변수를 숫자로 변환하지 않고, 주어진 값의 유형이 Number이고 값이 NaN이면 true, 아니면 false
  */
+
+function valueIsNaN(v) {
+  return v !== v;
+}
+console.log(valueIsNaN(1));
